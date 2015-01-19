829b35b0c89d95c43321761f33547cc987c50292
gumptionthomas <thomasbohmbach@gmail.com>
2015-01-18 20:54:01 -0600

    Better server queue error handling

--- server_queue.py
+++ server_queue.py
@@ -7,6 +7,7 @@ from google.appengine.api import taskqueue
 
 import gce
 
+NUM_RETRIES = 10
 START_EVENT = 'START_SERVER'
 STOP_EVENT = 'STOP_SERVER'
 BACKUP_EVENT = 'BACKUP_SERVER'
@@ -14,8 +15,21 @@ RESTART_EVENT = 'RESTART_SERVER'
 
 
 def queue_controller_task(payload):
-    payload = json.dumps(payload)
-    taskqueue.Queue('controller').add([taskqueue.Task(payload=payload, method='PULL')])
+    payload_json = json.dumps(payload)
+    retry = True
+    tries = 0
+    while retry:
+        try:
+            task = taskqueue.Task(payload=payload_json, method='PULL')
+            taskqueue.Queue('controller').add(task)
+            retry = False
+        except Exception as e:
+            tries += 1
+            if tries > NUM_RETRIES:
+                raise
+            logging.exception("Exception ({0}) queueing controller task {1}. Retrying...".format(
+                e, payload_json)
+            )
 
 
 def start_server(server, reserved_ports=None):
